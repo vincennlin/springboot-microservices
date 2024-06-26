7102fd8b96c1c50d364f353d06f5645b20278fc7
vincennlin <vincentagwa@gmail.com>
2024-06-26 20:37:17 +0800

    React Frontend - Design the Page to Display Employee, Department and Organization Details

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,7 +4,7 @@ import EmployeeComponent from './component/EmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <EmployeeComponent />
     </div>
   );
